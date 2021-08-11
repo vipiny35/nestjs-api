0c33fedf74c231c418bf88163aedae13bd1bc53d
Vipin Yadav <vipiny35@gmail.com>
2021-08-11 14:04:39 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-// }
-// bootstrap();
-
-(async () => {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs')
+    .setDescription('The  API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
-})();
+}
+bootstrap();
+
